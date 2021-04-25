b6417f66e3f1801d81e27fada63fcefc1e8539a5
Ashton Gasser <ashtongasser@gmail.com>
2021-04-25 15:13:10 -0500

    complete minus strectch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket =[];
+
+function addItem(item){
+  basket.push(item)
+  return true;
+}
+
+ function listItems(){
+   for(let i = 0; i<basket.length; i++){
+     console.log(basket[i])
+   }
+ }
+
+function empty(){
+  basket = [];
+}
+
+addItem('shirt')
+addItem('pants')
+addItem('jacket')
+listItems()
+empty()
+
+console.log(`basket is ${basket}`)
